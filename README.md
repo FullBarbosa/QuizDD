d1bc6edd5bfca725730dbe51dad22479231e9360
FullBarbosa <diibarbosa22@gmail.com>
2021-02-02 19:43:28 -0300

    finish

---
+++ README.md
@@ -0,0 +1,120 @@
+
+<div>
+  <h1 align="center">ALURA QUIZ</h1>
+  
+  <div align="center">
+
+  <a href="#sobre">Sobre</a> 
+    |
+  <a href="#tecnologias">Tecnólogias</a>
+    |
+  <a href="#dependencias">Dependências</a>
+    |
+  <a href="#Projeto">Pagina do projeto</a>
+
+  </div>
+</div>
+
+<div >
+<ul align="center">
+  <img src="https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E"
+        alt="Javascript">
+  <img src="https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB" alt="React.js">
+</ul>
+
+</div>
+
+
+<div>
+  <h3>Sobre.</h3>
+  <h4 id="sobre" > <b>Projeto feito em Next.js.</b>
+    Projeto voltado para uma interação com o usuario, onde o gamer "QUIZ" pode testar seus conhecimentos voltados a linguagem de programação JAVASCRIPT, de forma divertida e dinamica. </h4>
+
+  <img src="./src/img/ProjetoQuiz.gif" alt="Gif">
+</div>
+
+
+<div id="tecnologias">
+  <h3>Tecnológias.</h3>
+  <ul>
+    <li>
+      <a href="https://developer.mozilla.org/pt-BR/docs/Web/JavaScript">JavaScript.
+      </a>
+    </li>
+    <li>
+      <a href="https://styled-components.com/">
+        styled-components.
+      </a>
+    </li>
+    <li>
+      <a href="https://nextjs.org/">NEXT.js.
+      </a>
+    </li>
+    <li>
+      <a href="https://pt-br.reactjs.org/docs/getting-started.html">REACT.
+      </a>
+    </li>
+    <li>
+      <a href="https://www.framer.com/motion/">
+        Framer Motion.
+      </a>
+    </li>
+    <li>
+      <a href="https://nodejs.org/pt-br/docs/">
+        NODE.JS.
+      </a>
+    </li>
+    <li>
+      <a href="https://classic.yarnpkg.com/en/docs/">
+        YARN.
+      </a>
+    </li>
+  </ul>
+  
+</div>
+
+<div id="dependencias">
+  <h3>Dependêcias.</h3>
+    <p align="center">
+      para utilizar da aplicação, será necessario fazer alguns procedimentos de instalção de pacotes
+    </p>
+
+    ```bash
+# iniciar projeto Next.js
+$ yarn create next-app
+$ npx create-next-app
+
+# Install Framer Motion
+$ yarn add framer-motion
+$ npm install framer-motion
+
+#Styled-Components
+$ yarn add styled-components
+$ npm install styled-components
+
+```
+
+  <h3>Baixar Projeto.</h3>  
+```bash
+# Clone this repository
+$ git clone https://github.com/FullBarbosa/QuizJS.git QuizJS
+
+# Install dependencies Yarn
+$ yarn install
+
+# Install dependencies NPM
+$ npm install 
+
+```
+ </div>
+
+
+<div id="Projeto">
+  <h3>Projeto.</h3>
+  <a href="https://quiz-js-ten.vercel.app/">
+  <img width="200px" src="./src/img/QuizFoto.png" alt="Imagem de pagina web">
+  </a>
+</div>
+
+
+<h3>Orgulhosamente feito durante a imersão React/NEXT.js</h3>
--- pages/index.js
+++ pages/index.js
@@ -1,55 +1,133 @@
-import React, { useState } from 'react';
+/* eslint-disable import/no-unresolved */
+import React from 'react';
+import styled from 'styled-components';
+import Head from 'next/head';
+import { motion } from 'framer-motion';
 import { useRouter } from 'next/router';
+
 import db from '../db.json';
 import Widget from '../src/components/Widget';
+import Link from '../src/components/Link';
+import QuizLogo from '../src/components/QuizLogo';
 import QuizBackground from '../src/components/QuizBackground';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
 import Input from '../src/components/Input/Input';
 import Button from '../src/components/Button/Button';
-import QuizContainer from '../src/components/QuizContainer/QuizContainer';
+
+// const BackgroundImage = styled.div`
+//   background-image: url(${db.bg});
+//   flex: 1;
+//   background-size: cover;
+//   background-position: center;
+// `;
+
+const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
   const router = useRouter();
-  const [name, setName] = useState('');
-  return (
+  const [name, setName] = React.useState('');
 
+  return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>
+          AluraQuiz -
+          {db.title}
+        </title>
+      </Head>
       <QuizContainer>
-        <Widget>
+        <QuizLogo />
+        <Widget
+          as={motion.section}
+          transition={{ delay: 0, duration: 0.5 }}
+          variants={{
+            show: { opacity: 1, y: '0' },
+            hidden: { opacity: 0, y: '100%' },
+          }}
+          initial="hidden"
+          animate="show"
+        >
           <Widget.Header>
             <h1>{db.title}</h1>
           </Widget.Header>
           <Widget.Content>
             <p>{db.description}</p>
-          </Widget.Content>
-        </Widget>
-
-        <Widget>
-          <Widget.Content>
-            <form onSubmit={(event) => {
-              event.preventDefault();
+            <form onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
               router.push(`/quiz?name=${name}`);
+              console.log('Fazendo uma submissão por meio do react');
             }}
             >
               <Input
-                type="text"
+                name="nomeDoUsuario"
+                onChange={(infosDoEvento) => setName(infosDoEvento.target.value)}
                 placeholder="Diz ai seu nome"
-                onChange={({ target }) => { setName(target.value); }}
+                value={name}
               />
               <Button type="submit" disabled={name.length === 0}>
-                {' '}
-                Jogar
-                {' '}
-                {name}
+                {`Jogar ${name}`}
               </Button>
             </form>
           </Widget.Content>
         </Widget>
-        <Footer />
+
+        <Widget
+          as={motion.section}
+          transition={{ delay: 0.5, duration: 0.5 }}
+          variants={{
+            show: { opacity: 1 },
+            hidden: { opacity: 0 },
+          }}
+          initial="hidden"
+          animate="show"
+        >
+          <Widget.Content>
+            <h1>Quizes da Galera</h1>
+
+            <ul>
+              {db.external.map((linkExterno) => {
+                const [projectName, githubUser] = linkExterno
+                  .replace(/\//g, '')
+                  .replace('https:', '')
+                  .replace('.vercel.app', '')
+                  .split('.');
+
+                return (
+                  <li key={linkExterno}>
+                    <Widget.Topic
+                      as={Link}
+                      href={`/quiz/${projectName}___${githubUser}`}
+                    >
+                      {`${githubUser}/${projectName}`}
+                    </Widget.Topic>
+                  </li>
+                );
+              })}
+            </ul>
+          </Widget.Content>
+        </Widget>
+        <Footer
+          as={motion.footer}
+          transition={{ delay: 0.5, duration: 0.5 }}
+          variants={{
+            show: { opacity: 1 },
+            hidden: { opacity: 0 },
+          }}
+          initial="hidden"
+          animate="show"
+        />
       </QuizContainer>
       <GitHubCorner projectUrl="https://github.com/FullBarbosa" />
     </QuizBackground>
-
   );
 }
